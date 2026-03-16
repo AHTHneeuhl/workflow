dd9da9e51c81b27af6e5cf5839fcda7e6978598d
AHTHneeuhl <ahthneeuhl@gmail.com>
2026-03-17 00:15:16 +0530

    chore: add all shadcn ui components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      Hello World!
+    <div className="flex min-h-screen items-center font-extrabold text-5xl justify-center bg-zinc-50 font-serif  dark:bg-black">
+      <Button variant="ghost">Click Me</Button>
     </div>
   );
 }
