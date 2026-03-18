383341f65f81a3a7a590e2327b060b383124400b
AHTHneeuhl <ahthneeuhl@gmail.com>
2026-03-18 15:31:46 +0530

    feat: create AuthLayout component in features

---
+++ src/features/auth/components/auth-layout.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export function AuthLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Link
+          href="/"
+          className="flex items-center gap-2 self-center font-medium italic"
+        >
+          <Image alt="workflow" src="/logos/logo.svg" width={32} height={32} />
+          Workflow
+        </Link>
+        {children}
+      </div>
+    </div>
+  );
+}
