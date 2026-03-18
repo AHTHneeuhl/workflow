20d3df5136aaaef874bd2e3a9ddc086f336cd44a
AHTHneeuhl <ahthneeuhl@gmail.com>
2026-03-18 08:59:01 +0530

    feat: add login and regiter form components

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,9 @@
+import { RegisterForm } from "@/features/auth/components/register-form";
+
+export default function LoginPage() {
+  return (
+    <div>
+      <RegisterForm />
+    </div>
+  );
+}
---
+++ src/features/auth/components/login-form.tsx
@@ -0,0 +1,161 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import {
+  Field,
+  FieldGroup,
+  FieldLabel,
+  FieldError,
+} from "@/components/ui/field";
+
+import { Input } from "@/components/ui/input";
+// import { authClient } from "@/lib/auth-client";
+
+const loginSchema = z.object({
+  email: z.email("Please enter a valid email address"),
+  password: z.string().min(1, "Password is required"),
+});
+
+type LoginFormValues = z.infer<typeof loginSchema>;
+
+export function LoginForm() {
+  const router = useRouter();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<LoginFormValues>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (values: LoginFormValues) => {
+    console.log(values);
+    // await authClient.signIn.email(
+    //   {
+    //     email: values.email,
+    //     password: values.password,
+    //     callbackURL: "/",
+    //   },
+    //   {
+    //     onSuccess: () => {
+    //       router.push("/");
+    //     },
+    //     onError: (ctx) => {
+    //       toast.error(ctx.error.message);
+    //     },
+    //   },
+    // );
+  };
+
+  return (
+    <div className="flex flex-col gap-6">
+      <Card>
+        <CardHeader className="text-center">
+          <CardTitle>Welcome back</CardTitle>
+          <CardDescription>Login to continue</CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          <form onSubmit={handleSubmit(onSubmit)} className="grid gap-6">
+            {/* Social Buttons */}
+            <div className="flex flex-col gap-4">
+              <Button
+                variant="outline"
+                className="w-full gap-2"
+                type="button"
+                disabled={isSubmitting}
+              >
+                <Image
+                  alt="GitHub"
+                  src="/logos/github.svg"
+                  width={20}
+                  height={20}
+                />
+                Continue with GitHub
+              </Button>
+
+              <Button
+                variant="outline"
+                className="w-full gap-2"
+                type="button"
+                disabled={isSubmitting}
+              >
+                <Image
+                  alt="Google"
+                  src="/logos/google.svg"
+                  width={20}
+                  height={20}
+                />
+                Continue with Google
+              </Button>
+            </div>
+
+            {/* Fields */}
+            <FieldGroup>
+              {/* Email */}
+              <Field>
+                <FieldLabel htmlFor="email">Email</FieldLabel>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  {...register("email")}
+                />
+                {errors.email && (
+                  <FieldError>{errors.email.message}</FieldError>
+                )}
+              </Field>
+
+              {/* Password */}
+              <Field>
+                <FieldLabel htmlFor="password">Password</FieldLabel>
+                <Input
+                  id="password"
+                  type="password"
+                  placeholder="*********"
+                  {...register("password")}
+                />
+                {errors.password && (
+                  <FieldError>{errors.password.message}</FieldError>
+                )}
+              </Field>
+
+              <Button type="submit" className="w-full" disabled={isSubmitting}>
+                {isSubmitting ? "Logging in..." : "Login"}
+              </Button>
+            </FieldGroup>
+
+            {/* Footer */}
+            <div className="text-center text-sm">
+              Don&apos;t have an account?{" "}
+              <Link href="/register" className="underline underline-offset-4">
+                Register
+              </Link>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
