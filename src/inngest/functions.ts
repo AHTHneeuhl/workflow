144c80d40838cb2151aa42dd0ed443219f1f57d0
AHTHneeuhl <ahthneeuhl@gmail.com>
2026-03-19 12:20:06 +0530

    feat: add openai and anthropic

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,18 +1,50 @@
 import { createGoogleGenerativeAI } from "@ai-sdk/google";
+import { createOpenAI } from "@ai-sdk/openai";
+import { createAnthropic } from "@ai-sdk/anthropic";
 import { generateText } from "ai";
 import { inngest } from "./client";
 
 const google = createGoogleGenerativeAI();
+const openai = createOpenAI();
+const anthropic = createAnthropic();
 
 export const executeAi = inngest.createFunction(
   { id: "execute-ai", triggers: [{ event: "execute.ai" }] },
   async ({ event, step }) => {
-    const { steps } = await step.ai.wrap("gemini-generate-text", generateText, {
-      model: google("gemini-2.5-flash"),
-      system: "You are a helpful assistant.",
-      prompt: "What is the meaning of life?",
-    });
+    const { steps: geminiSteps } = await step.ai.wrap(
+      "gemini-generate-text",
+      generateText,
+      {
+        model: google("gemini-2.5-flash"),
+        system: "You are a helpful assistant.",
+        prompt: "What is the meaning of life?",
+      },
+    );
 
-    return steps;
+    const { steps: openAiSteps } = await step.ai.wrap(
+      "openai-generate-text",
+      generateText,
+      {
+        model: openai("gpt-5.2"),
+        system: "You are a helpful assistant.",
+        prompt: "What is the meaning of life?",
+      },
+    );
+
+    const { steps: anthropicSteps } = await step.ai.wrap(
+      "anthropic-generate-text",
+      generateText,
+      {
+        model: anthropic("claude-sonnet-4-5"),
+        system: "You are a helpful assistant.",
+        prompt: "What is the meaning of life?",
+      },
+    );
+
+    return {
+      geminiSteps,
+      openAiSteps,
+      anthropicSteps,
+    };
   },
 );
