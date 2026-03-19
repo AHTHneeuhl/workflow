5b83b3cf76c554e14317bda51cfe7896f5c10dfd
AHTHneeuhl <ahthneeuhl@gmail.com>
2026-03-19 10:13:21 +0530

    feat: add inngest background jobs workflow trigger with function hello.world

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,30 @@
-import { requireAuth } from "@/lib/auth-utils";
+"use client";
 
-export default async function Home() {
-  await requireAuth();
+import { Button } from "@/components/ui/button";
+import { requireAuth } from "@/lib/auth-utils";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+
+export default function Home() {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
+
+  const create = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => {
+        queryClient.invalidateQueries(trpc.getWorkflows.queryOptions());
+      },
+    }),
+  );
 
   return (
-    <div className="min-h-screen min-w-screen flex items-center justify-center">
+    <div className="min-h-screen min-w-screen flex items-center flex-col gap-6 justify-center">
       protected server component
+      <div>{JSON.stringify(data, null, 2)}</div>
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>
+        Create Workflow
+      </Button>
     </div>
   );
 }
