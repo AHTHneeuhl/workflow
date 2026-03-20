bd7cfcb8df7d585388e73f7127ebf0fd826fe12f
AHTHneeuhl <ahthneeuhl@gmail.com>
2026-03-20 11:42:18 +0530

    feat: add prefetch, hydrateClient and render workflowsList component

--- src/app/(dashboard)/(rest)/workflows/page.tsx
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -1,7 +1,21 @@
+import { WorkflowsList } from "@/features/workflows/components/workflows";
+import { prefetchWorkflows } from "@/features/workflows/servers/prefetch";
 import { requireAuth } from "@/lib/auth-utils";
+import { HydrateClient } from "@/trpc/server";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 export default async function WorkflowsPage() {
   await requireAuth();
 
-  return <div>Workflows</div>;
+  prefetchWorkflows();
+
+  return (
+    <HydrateClient>
+      <ErrorBoundary fallback={<p>Something went wrong!</p>}></ErrorBoundary>
+      <Suspense fallback={<p>Loading...</p>}>
+        <WorkflowsList />
+      </Suspense>
+    </HydrateClient>
+  );
 }
