f4f24812215277a599fda5125aedc5778d16558d
AHTHneeuhl <ahthneeuhl@gmail.com>
2026-03-20 11:11:34 +0530

    feat: add workflows model and create workflow crud routers

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,33 +1,8 @@
-import prisma from "@/lib/prisma";
-import {
-  createTRPCRouter,
-  premiumProcedure,
-  protectedProcedure,
-} from "../init";
-import { inngest } from "@/inngest/client";
+import { workflowsRouter } from "@/features/workflows/servers/routers";
+import { createTRPCRouter } from "../init";
 
 export const appRouter = createTRPCRouter({
-  getWorkflows: protectedProcedure.query(({ ctx }) => {
-    return prisma.workflow.findMany();
-  }),
-  createWorkflow: protectedProcedure.mutation(async () => {
-    await inngest.send({
-      name: "test/hello.world",
-      data: {
-        email: "test@example.com",
-      },
-    });
-
-    return { success: true, message: "Job queued" };
-  }),
-
-  testAi: premiumProcedure.mutation(async () => {
-    await inngest.send({
-      name: "execute.ai",
-    });
-
-    return { success: true, message: "Job queued" };
-  }),
+  workflows: workflowsRouter,
 });
 
 // export type definition of API
