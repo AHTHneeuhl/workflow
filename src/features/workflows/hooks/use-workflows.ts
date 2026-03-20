370f6bf49b28be8b90e9d7007617e6bd72c81bd5
AHTHneeuhl <ahthneeuhl@gmail.com>
2026-03-20 14:46:06 +0530

    feat: create entity-component, upgrade-modal, use-upgrade-modal, use-workflows hooks

--- src/features/workflows/hooks/use-workflows.ts
+++ src/features/workflows/hooks/use-workflows.ts
@@ -1,5 +1,10 @@
 import { useTRPC } from "@/trpc/client";
-import { useSuspenseQuery } from "@tanstack/react-query";
+import {
+  useMutation,
+  useQueryClient,
+  useSuspenseQuery,
+} from "@tanstack/react-query";
+import { toast } from "sonner";
 
 /**
  * Hook to fetch all workflows using suspense
@@ -8,4 +13,23 @@ export const useSuspenseWorkflows = () => {
   const trpc = useTRPC();
 
   return useSuspenseQuery(trpc.workflows.getMany.queryOptions());
+};
+
+/**
+ * Hook to create a new workflow
+ */
+export const useCreateWorkflow = () => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+
+  return useMutation(
+    trpc.workflows.create.mutationOptions({
+      onSuccess: (workflow) => {
+        toast.success(`Workflow ${workflow.name} created`);
+        queryClient.invalidateQueries(trpc.workflows.getMany.queryOptions());
+      },
+      onError: (error) =>
+        toast.error(`Failed to create workflow: ${error.message}`),
+    }),
+  );
 };
